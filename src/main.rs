453abc94d73e92e3d5141cd0366fb7dbf323be8b
Rafael Sa <rafaeljdsb1@gmail.com>
2025-08-17 08:18:46 -0300

    refactor to lib

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,6 @@
-mod config;
-mod config_loader;
-mod db;
-mod feature_flags;
-mod handlers;
-mod models;
-mod repository;
-mod schema;
-mod swapi;
-
-use actix_web::{App, HttpServer, web};
-use config_loader::load_settings;
-use db::create_pool;
-
-use crate::{feature_flags::FeatureFlagManager, repository::Repository};
-use actix_ws::Session;
-use tokio::sync::RwLock;
-
-pub struct AppState {
-    pub sessions: RwLock<Vec<Session>>,
-}
+use swapi_rust::run_server;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let settings = load_settings();
-    let pool = create_pool(&settings.db.url);
-    let repository = web::Data::new(Repository::new(pool));
-    let feature_flag_manager = web::Data::new(FeatureFlagManager::new().await);
-    let app_state = web::Data::new(AppState {
-        sessions: RwLock::new(Vec::new()),
-    });
-
-    let address = "127.0.0.1";
-    let port = 8080;
-    let server = HttpServer::new(move || {
-        App::new()
-            .app_data(repository.clone())
-            .app_data(feature_flag_manager.clone())
-            .app_data(app_state.clone())
-            .service(handlers::get_planet_by_id)
-            .service(handlers::ws_connect)
-    })
-    .bind((address, port))?;
-
-    println!("🚀 Application is running at http://{address}:{port}");
-
-    server.run().await
+    run_server().await
 }
